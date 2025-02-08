72f16bb101acf31a7898d86ebb76989dd2517a01
rizaievdima <rizaievdima@gmail.com>
2025-02-08 16:50:53 +0200

    Added new files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('Happy developing ✨')
+
+
+const data = {
+
+
+    age: 20, // Add a comma here
+    blender: {
+        brand: 'Generic',
+        model: 'X200',
+        power: 500, // in watts
+        color: 'Silver'
+    }
+}
+
